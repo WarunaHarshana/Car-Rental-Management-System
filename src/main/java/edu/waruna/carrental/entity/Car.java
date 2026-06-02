366cbd0c28cec61668ddb92f5be546f6f006ad5a
Waruna <warunaharshana78@gmail.com>
2026-06-02 18:29:31 +0530

    add Car entity with JPA annotations

---
+++ src/main/java/edu/waruna/carrental/entity/Car.java
@@ -0,0 +1,39 @@
+package edu.waruna.carrental.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cars")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(name = "fuel_type", nullable = false)
+    private String fuelType;
+
+    @Column(name = "seating_capacity", nullable = false)
+    private Integer seatingCapacity;
+
+    @Column(name = "daily_price", nullable = false)
+    private Double dailyPrice;
+
+    @Column(nullable = false)
+    private String status; // AVAILABLE, RENTED, MAINTENANCE
+
+}
